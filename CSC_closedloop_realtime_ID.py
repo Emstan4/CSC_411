b9c7b4d7cdb136a83d2ffb4cd6517b5c90ee2208
Emstan4 <u12284922@tuks.co.za>
2016-04-07 13:27:51 +0200

    Fixing problems with quality and convergence

--- CSC_closedloop_realtime_ID.py
+++ CSC_closedloop_realtime_ID.py
@@ -43,7 +43,7 @@ z_cont = np.zeros((contNstates, 1))
 z = np.zeros((Nstates, 1))
 
 t_start = 0
-t_end = 100
+t_end = 1000
 dt = 0.01
 tspan = np.arange(t_start, t_end, dt)
 npoints = len(tspan)
@@ -51,8 +51,6 @@ npoints = len(tspan)
 #List for storages
 yplot = []
 phi_T = []
-#sigma
-deviation = 0.0
 #Initial conditions
 z = 0
 y = 0
@@ -77,7 +75,7 @@ my_sum = np.zeros((2,2))
 my_sum2 = np.zeros((2,1))
 y_list = []
 
-deviation = 1.0
+deviation = 0.2
 next_time3 = 0
 
 #Real time initialization
@@ -108,14 +106,21 @@ R = []
 R2 = []
 quality = 0
 quality2 = 0
+tplot = []
+w_1 = 0
+w_list = []
+disturbance = 0
 for i, t in enumerate(tspan):
-    
+    w = u + disturbance
     noise = deviation*np.random.rand()
+    w_list.append(w)
+    yplot.append(y)
+    tplot.append(t)
     if t >= next_time3:
             cnt = (-1)**j
-            y_sp += 2*cnt
+            disturbance += cnt
             j += 1 
-            delta2 = 5
+            delta2 = 10
             
             next_time3 += delta2
             
@@ -123,6 +128,7 @@ for i, t in enumerate(tspan):
         t_plot.append(t)
         Qplot_1.append(Q_0[0,0])
         Qplot_2.append(Q_0[1,0])
+        
         a_list.append(a)
         b_list.append(b)
         R.append(quality)
@@ -140,7 +146,7 @@ for i, t in enumerate(tspan):
         cntA += 1
         if t >= next_time2:
                 
-            phi_T.append([y_1, u_1])
+            phi_T.append([y_1, w_1])
             
             phi = np.matrix.transpose(np.array(phi_T))
             y_list.append([y])
@@ -159,7 +165,7 @@ for i, t in enumerate(tspan):
             P_0 = P_t
             
             #Quality determination
-            V_Q = np.dot(np.matrix.transpose(y_list - np.dot(phi_T, Q_t)), y_list - np.dot(phi_T, Q_t))
+            
             error = abs(Q_real - Q_t)
             
             error_1[cntA] = error[1,0]
@@ -180,6 +186,7 @@ for i, t in enumerate(tspan):
         int_listA = []
         int_listB = []
         y_1 = y
+        w_1 = w
         u_1 = u
         phi_T = []
         y_list = []
@@ -187,29 +194,31 @@ for i, t in enumerate(tspan):
         
         next_time += delta
         
-    yplot.append(y)
+    
     e = y_sp - y
     
     dzcdt = contA*z_cont + contB*e
     u = contC*z_cont + contD*e
     u = u[0,0]
         
-    dzdt = A*z + B*u
-    y = C*z + D*u
+    dzdt = A*z + B*w
+    y = C*z + D*w
     y = y[0,0]
     y += noise
     
     z += dzdt*dt
     z_cont += dzcdt*dt
     
-plot.subplot(2,2,1)    
-plot.plot(t_plot,R)
+plot.subplot(2,2,4)    
+plot.plot(tplot,w_list)
+
 plot.subplot(2,2,3) 
 plot.plot(t_plot, Qplot_2, t_plot, b_list)  
 
 plot.subplot(2,2,2)    
-plot.plot(t_plot,R2)
-plot.subplot(2,2,4) 
+plot.plot(tplot,yplot)
+
+plot.subplot(2,2,1) 
 plot.plot(t_plot, Qplot_1, t_plot, a_list) 
      
 plot.show() 
