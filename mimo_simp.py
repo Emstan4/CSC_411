bb7b83558f240438341751ce03401167df0974cc
emstan4 <u12284922@tuks.co.za>
2016-04-20 16:46:22 +0200

    added quality analysis

--- mimo_simp.py
+++ mimo_simp.py
@@ -15,11 +15,10 @@ def step(start, step, tstep, t):
     else:
         return start
         
-
 u_1, u ,v, v_1, y_1, z_1, y, z = 0,0,0,0,0,0,0,0
 outputs = []
 tstart = 0
-tend = 100
+tend = 200
 T = 1.0
 tspan = np.arange(tstart, tend, T)
 
@@ -44,10 +43,9 @@ K = 1.
 tau2_i = 1
 tau2_d = 0.6
 e_2, e_1,  e, e2_2, e2_1, e2 = [0,0,0,0,0,0]
-sigma = 0.05
+sigma = 0.03
 #ID
 sigma2 = 1000000000000
-
 Q_0 = np.zeros((3,1))
 P_0 = sigma2*np.eye(3)
 Q2_0 = np.zeros((3,1))
@@ -65,11 +63,20 @@ j = 0
 ysp = 0.5
 ysp2 = 0.1
 yk_1, yk, zk_1, zk = [0,0,0,0]
-   
-for t in tspan:
-    
+
+recent_error_list = []
+counter = 0 
+
+error = np.zeros(len(tspan))
+error2 = np.zeros(len(tspan))  
+quality = []
+setpoints = []
+tplot = []
+for i, t in enumerate(tspan):
+    tplot.append(t)
     noise = sigma*np.random.rand()
     noise2 = sigma*np.random.rand()
+    disturbance = step(0., 0., 200, t)
     if t >= next_time:
         cnt = (-1)**j
         ysp += 1*cnt 
@@ -79,10 +86,12 @@ for t in tspan:
         next_time += delta2
     
     outputs.append([y,z,yk,zk])
+    
     Q_real.append([b,a_1,a_2,b_2,a_3,a_4])    
     Q_esti.append(Q_0.T[0])
     Q2_esti.append(Q2_0.T[0])
-#----------------DIENTIFICATION---------------------------------------------
+    setpoints.append([ysp, ysp2])
+#----------------IDENTIFICATION---------------------------------------------
 
     phi_T.append([y_1, u_1, v_1])
     phi2_T.append([z_1, u_1, v_1])
@@ -124,8 +133,7 @@ for t in tspan:
  #---------------------------------------------------------------------------   
     e_2 = e_1
     e_1 = e
-    e = ysp - y
-    
+    e = ysp - y    
     e2_2 = e2_1
     e2_1 = e2
     e2 = ysp2 - z
@@ -143,19 +151,48 @@ for t in tspan:
     yk = Q_t[0,0]*yk_1 + Q_t[1,0]*u_1 + Q_t[2,0]*v_1    
     zk = Q2_t[0,0]*zk_1 + Q2_t[1,0]*u_1 + Q2_t[2,0]*v_1 
     
-    y = b*y_1 + a_1*u_1 + a_2*v_1  + noise    
+    y = b*y_1 + a_1*u_1 + a_2*v_1  + noise + disturbance   
     z = b_2*z_1 + a_3*u_1 + a_4*v_1 + noise2
         
+    error[i] = (abs(np.array(outputs)[:,0] - np.array(outputs)[:,2])[i]) # error in the y-outputs(first)
+    error2[i] = abs(np.array(outputs)[:,1] - np.array(outputs)[:,3])[i] # error in thez-outputs(second)
+    for k in range(20):
+            recent_error_list.append([error[counter - k], error2[counter - k]])  
+                
+    min_quality = np.min([1 - np.sum(recent_error_list[0]), 1 - np.sum(recent_error_list[1])])           
+    
+    quality.append([(1 - np.sum(recent_error_list[0]))**2, (1 - np.sum(recent_error_list[1]))**2, min_quality])
+    
+    recent_error_list = []
+    counter += 1
+    if t >= T:
+        if min_quality >= 0.98:
+            break
 Q_esti = np.array(Q_esti) 
-Q_real = np.array(Q_real)   
+quality = np.array(quality)
+Q_real = np.array(Q_real)
+setpoints = np.array(setpoints)   
 outputs = np.array(outputs)
- 
-plot.subplot(2,1,1)
+tspan = tplot
+plot.subplot(2,2,1)
+plot.plot(tspan, quality[:,2])
+plot.ylabel('quality')
+plot.subplot(2,2,2)
 plot.plot(tspan, outputs)
+plot.plot(tspan, setpoints)
 plot.ylabel('outputs')
-plot.subplot(2,1,2)
+plot.subplot(2,2,3)
 plot.plot(tspan, Q_esti, tspan, Q2_esti)
 plot.plot(tspan, Q_real, 'k')
 plot.xlabel('time')
 plot.ylabel('Parameters')
+plot.subplot(2,2,4)
+plot.plot(tspan, setpoints)
+plot.ylabel('setpoints')
+plot.xlabel('time')
+plot.plot()
 plot.show()
+
+print Q_esti[-1], Q2_esti[-1]
+print Q_real[-1]
+print tspan[-1]
