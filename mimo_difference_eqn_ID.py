89cefc16dc8241e48c16b28d3e37fa9211b92368
emstan4 <u12284922@tuks.co.za>
2016-05-09 08:33:00 +0200

    adding offline estimation to be compared with online estimation

--- mimo_difference_eqn_ID.py
+++ mimo_difference_eqn_ID.py
@@ -9,7 +9,7 @@ from __future__ import division
 import numpy as np
 from matplotlib import pyplot as plot
 import scipy
-import random
+import csv
 
 def step(start, step, tstep, t):
     if t >= tstep:
@@ -19,7 +19,26 @@ def step(start, step, tstep, t):
 def square_wave(width, t):
     period = (np.pi)/width
     return scipy.signal.square(period*t, duty = 0.5)
+    
+def writefile(data, filename):        
+    with open(filename, 'wb') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=",")
+        for row_data in data:
+            csvwriter.writerow(row_data)
         
+def readfile(filename):        
+    data2 = []
+    with open(filename, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        for cnt, row in enumerate(csvreader):
+            data2.append(float(row[0]))
+    return data2 
+    
+para_offlineA =  readfile('off_para1.csv') 
+para_offlineB =  readfile('off_para2.csv') 
+para_offlineA = np.array([para_offlineA])
+para_offlineB = np.array([para_offlineB])
+  
 #process parameters
 tau_1, K_1 = 1,1
 tau_2, K_2 = 5,0.5
@@ -57,7 +76,7 @@ ee = a_2
 ff = -a_2*b_4
 
 #lists        
-outputs = np.zeros((npoints, 4))
+outputs = np.zeros((npoints, 6))
 inputs = []
 controller_outputs = np.zeros((npoints, 4))
 para_estim = np.zeros((npoints,6))
@@ -74,7 +93,8 @@ alp, alp_1, alp_2 = 0,0,0
 bet, bet_1, bet_2 = 0,0,0
 yk, yk_1, yk_2 = 0,0,0
 zk, zk_1, zk_2 = 0,0,0
-
+m, m_1, m_2 = 0,0,0
+n, n_1, n_2 = 0,0,0
 #controller
 Kc = 0.1
 tau_i = 5
@@ -116,12 +136,18 @@ error_sum = 1.0
 np.random.seed(seed=0)
 dist = 0.0
 dist2 = 0
+
+my_sumA_1 = np.zeros((6,6))
+my_sumA_2 = np.zeros((6,1))
+my_sumB_1 = np.zeros((6,6))
+my_sumB_2 = np.zeros((6,1))
+
 for i, t in enumerate(tspan):
     
     noise = sigma*np.random.rand()
     noise2 = sigma*np.random.rand()
     
-    outputs[i] = [y, z, yk, zk]
+    outputs[i] = [y, z, yk, zk, m, n]
     inputs.append([ysp, ysp2, alp, bet])
     para_estim[i] = Q_0.T[0]
     para_estim2[i] = Q2_0.T[0]
@@ -146,6 +172,17 @@ for i, t in enumerate(tspan):
     
     y_list.append([y])
     z_list.append([z])
+    
+    productA_1 = np.dot(phi, phi_T)
+    productA_2 = np.dot(phi, y_list)
+    my_sumA_1 += productA_1
+    my_sumA_2 += productA_2    
+    
+    
+    productB_1 = np.dot(phi2, phi2_T)
+    productB_2 = np.dot(phi2, z_list)
+    my_sumB_1 += productB_1
+    my_sumB_2 += productB_2
     
     alpha = np.dot(np.dot(P_0,phi),np.dot(phi_T,P_0))
     beta = lambd + np.dot(np.dot(phi_T,P_0),phi)
@@ -181,7 +218,7 @@ for i, t in enumerate(tspan):
         q_sampled = Q_t
         q_sampled2 = Q2_t
         
-        next_time2 += 10
+        next_time2 += 20
     quality_list.append((1 - error_sum))                
     e_2 = e_1
     e_1 = e
@@ -212,7 +249,12 @@ for i, t in enumerate(tspan):
     yk_2 = yk_1
     yk_1 = yk
     zk_2 = zk_1
-    zk_1 = zk         
+    zk_1 = zk   
+    m_2 = m_1
+    m_1 = m
+    n_2 = n_1
+    n_1 = n
+      
     y = np.dot([[y_1, y_2, alp_1, alp_2, bet_1, bet_2]],[[a],[b],[c],[d],[e],[f]])
     z = np.dot([[z_1, z_2, alp_1, alp_2, bet_1, bet_2]],[[aa],[bb],[cc],[dd],[ee],[ff]])
     y = y[0,0] + noise
@@ -221,10 +263,11 @@ for i, t in enumerate(tspan):
     yk = yk[0,0]
     zk = np.dot([[zk_1, zk_2, alp_1, alp_2, bet_1, bet_2]], q_sampled2)  
     zk = zk[0,0] 
+    m = np.dot([m_1, m_2, alp_1, alp_2, bet_1, bet_2], para_offlineA.T)
+    n = np.dot([n_1, n_2, alp_1, alp_2, bet_1, bet_2], para_offlineB.T)
     
     error = abs(yk - y)
     error_list.append(error)
-    
     phi_T = []
     y_list = []
     phi2_T = []
@@ -233,6 +276,16 @@ for i, t in enumerate(tspan):
 #print q_sampled.T
 #print np.array([[a],[b],[c],[d],[e],[f]]).T   
 #plot.plot(quality_list) 
+my_sumA_inv = np.linalg.inv(my_sumA_1)
+parameters = np.dot(my_sumA_inv, my_sumA_2)  
+
+my_sumB_inv = np.linalg.inv(my_sumB_1)
+parametersB = np.dot(my_sumB_inv, my_sumB_2) 
+
+    
+writefile(parameters, "off_para1.csv")
+writefile(parametersB, "off_para2.csv")
+
 outputs = np.array(outputs)
 inputs = np.array(inputs)
 para_real = np.array(para_real)
@@ -240,8 +293,10 @@ para_estim = np.array(para_estim)
 plot.subplot(2,2,1)
 plot.plot(tspan, outputs[:,0], label = "$y$")
 plot.plot(tspan, outputs[:,1], label = "$z$")
-plot.plot(tspan, outputs[:,2], label = "$y_{predicted}$")
-plot.plot(tspan, outputs[:,3], label = "$z_{predicted}$")
+plot.plot(tspan, outputs[:,2], label = "$y_{predicted}$", alpha = 0.5)
+plot.plot(tspan, outputs[:,3], label = "$z_{predicted}$", alpha = 0.5)
+plot.plot(tspan, outputs[:,4], label = "$z_{predicted}$")
+plot.plot(tspan, outputs[:,5], label = "$z_{predicted}$")
 plot.ylabel("outputs")
 #plot.legend(loc = "best")
 plot.subplot(2,2,2)
